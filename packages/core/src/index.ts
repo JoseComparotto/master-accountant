5b67acc6f0598d171cc25d2a6b570d03c6a31eda
José Comparotto <jose.comparotto@gmail.com>
2026-06-06 00:42:27 -0300

    feat: add generate-barrels script for automatic index generation; update package.json and turbo.json tasks

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,15 +1,16 @@
-// TODO: Automatizar geração do index
+// Ficheiro gerado automaticamente pelo pipeline. Não editar manualmente.
 
-export * from './modules/chart-of-accounts/entities/account.entity.js'
-export * from './modules/chart-of-accounts/exceptions/account.exception.js'
-export * from './modules/chart-of-accounts/enums/account-class.enum.js'
-export * from './modules/chart-of-accounts/enums/balance-type.enum.js'
-export * from './modules/chart-of-accounts/interfaces/account-repository.interface.js'
-export * from './modules/chart-of-accounts/interfaces/hierarchy-checker.interface.js'
-export * from './modules/chart-of-accounts/services/account-domain.service.js'
-export * from './modules/chart-of-accounts/services/hierarchy-checker.service.js'
-export * from './modules/chart-of-accounts/value-objects/structural-code.value.js'
-export * from './modules/chart-of-accounts/value-objects/account-name.value.js'
-export * from './shared/helpers/value-objects.helper.js'
-export * from './shared/exception/domain.exception.js'
-export * from './shared/value-objects/uuid.value.js'
+export * from './shared/exception/domain.exception.js';
+export * from './shared/helpers/assert.helper.js';
+export * from './shared/helpers/value-objects.helper.js';
+export * from './shared/value-objects/uuid.value.js';
+export * from './modules/chart-of-accounts/entities/account.entity.js';
+export * from './modules/chart-of-accounts/enums/account-class.enum.js';
+export * from './modules/chart-of-accounts/enums/balance-type.enum.js';
+export * from './modules/chart-of-accounts/exceptions/account.exception.js';
+export * from './modules/chart-of-accounts/interfaces/account-repository.interface.js';
+export * from './modules/chart-of-accounts/interfaces/hierarchy-checker.interface.js';
+export * from './modules/chart-of-accounts/services/account-domain.service.js';
+export * from './modules/chart-of-accounts/services/hierarchy-checker.service.js';
+export * from './modules/chart-of-accounts/value-objects/account-name.value.js';
+export * from './modules/chart-of-accounts/value-objects/structural-code.value.js';
