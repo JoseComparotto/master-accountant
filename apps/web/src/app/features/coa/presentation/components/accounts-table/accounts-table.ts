e76662afd502e0dd370b690b37a7c69c6150e6d7
José Comparotto <jose.comparotto@gmail.com>
2026-06-19 20:55:15 -0300

    feat: enhance accounts table with collapsible rows and toggle active state functionality

--- apps/web/src/app/features/coa/presentation/components/accounts-table/accounts-table.ts
+++ apps/web/src/app/features/coa/presentation/components/accounts-table/accounts-table.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { AccountDto } from '@repo/coa-contracts';
+import { ChangeDetectionStrategy, Component, computed, input, model, output } from '@angular/core';
+import { AccountDto, AccountNodeDto } from '@repo/coa-contracts';
 import { ZardTableComponent, ZardTableHeaderComponent, ZardTableBodyComponent, ZardTableHeadComponent, ZardTableRowComponent, ZardTableCellComponent } from "@/shared/presentation/components/table";
 import { NgClass } from '@angular/common';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { lucideCopy, lucideEye } from '@ng-icons/lucide';
+import { lucideChevronDown, lucideChevronRight, lucideCopy, lucideEye, lucideEyeOff } from '@ng-icons/lucide';
 import { ZardButtonComponent } from '@/shared/presentation/components/button';
 import { AccountTheme } from '../../directives/account-theme';
 import { AccountTitle } from '../account-title/account-title';
@@ -26,13 +26,57 @@ import { AccountTitle } from '../account-title/account-title';
   styleUrl: './accounts-table.css',
 
   changeDetection: ChangeDetectionStrategy.OnPush,
-  viewProviders: [provideIcons({ lucideCopy, lucideEye })],
+  viewProviders: [provideIcons({
+    lucideChevronDown,
+    lucideChevronRight,
+    lucideEye,lucideEyeOff
+  })],
 })
 export class AccountsTable {
+  tree = input.required<AccountNodeDto[]>();
 
-  accounts = input.required<AccountDto[]>();
+  toggleActive = output<AccountNodeDto>();
+
+  showInactive = model(true);
+  collapsedIds = model<Set<string>>(new Set());
+
+  rows = computed(() => {
+    const rows: AccountNodeDto[] = [];
+    const walk = (nodes: AccountNodeDto[]) => {
+      for (const n of nodes) {
+        if (!this.showInactive() && !n.isActive) continue;
+        rows.push(n);
+        if (!this.collapsedIds().has(n.id) && n.children)
+          walk(n.children);
+      }
+    };
+    walk(this.tree());
+    return rows;
+  });
 
   getWeigthClass(isSummary: boolean): string {
     return isSummary ? 'font-semibold' : '';
   }
+
+  hasChildren(account: AccountNodeDto) {
+    return account.children && account.children?.length > 0;
+  }
+
+  isCollepsed(id: string) {
+    return this.collapsedIds().has(id);
+  }
+
+  toggleCollapsed(id: string) {
+    this.collapsedIds.update(prev => {
+      const next = new Set(prev);
+
+      if (prev.has(id))
+        next.delete(id);
+      else
+        next.add(id);
+
+      return next;
+    })
+  }
+  
 }
--- apps/web/src/app/features/coa/presentation/pages/accounts-page/accounts-page.ts
+++ apps/web/src/app/features/coa/presentation/pages/accounts-page/accounts-page.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { AccountsTable } from '../../components/accounts-table/accounts-table';
-import { AccountDto } from '@repo/coa-contracts';
+import { AccountNodeDto } from '@repo/coa-contracts';
 import { CoaApiClient } from '../../../infrastructure/coa-api-client';
 
 @Component({
@@ -10,19 +10,24 @@ import { CoaApiClient } from '../../../infrastructure/coa-api-client';
   styleUrl: './accounts-page.css',
 })
 export class AccountsPage implements OnInit {
+
   private client = inject(CoaApiClient);
 
   protected loading = signal(false);
-  protected accounts = signal<AccountDto[]>([]);
+  protected accountsTree = signal<AccountNodeDto[]>([]);
 
   async ngOnInit() {
+    await this.loadAccounts();
+  }
+
+  async loadAccounts() {
     this.loading.set(true);
     try {
-      const { status, body } = await this.client.accounts.getAll();
+      const { status, body } = await this.client.accounts.getTree();
 
       switch (status) {
         case 200:
-          this.accounts.set(body)
+          this.accountsTree.set(body)
           break;
 
         default:
@@ -30,12 +35,30 @@ export class AccountsPage implements OnInit {
           throw new Error("Não foi possivel buscar contas.");
       }
 
-    } catch (e){
+    } catch (e) {
       console.error("Erro ao buscar contas.");
       throw e;
     }
     finally {
       this.loading.set(false);
     }
+  }
+
+  async toggleActive(account: AccountNodeDto) {
+    let promise;
+    if (account.isActive)
+      promise = this.client.accounts.inactivate({ params: { id: account.id } });
+    else
+      promise = this.client.accounts.activate({ params: { id: account.id } });
+
+    const { status, body } = await promise;
+
+    if (status === 200){
+      this.loadAccounts();
+      return;
+    }
+    if (status === 422) throw new Error(body.message);
+    console.error("Erro inesperado: ", body);
+    throw new Error("Erro inesperado ao anternar estado da conta.");
   }
 }
