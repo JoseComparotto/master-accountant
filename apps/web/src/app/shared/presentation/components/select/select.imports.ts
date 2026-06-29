0d9049e9e1b43a6cb3dcfdc9cc290acb3915853a
José Comparotto <jose.comparotto@gmail.com>
2026-06-29 18:01:53 -0300

    refator: removido componente não utilizado

--- apps/web/src/app/shared/presentation/components/select/select.imports.ts
+++
@@ -1,4 +0,0 @@
-import { ZardSelectItemComponent } from '@/shared/presentation/components/select/select-item.component';
-import { ZardSelectComponent } from '@/shared/presentation/components/select/select.component';
-
-export const ZardSelectImports = [ZardSelectComponent, ZardSelectItemComponent] as const;
