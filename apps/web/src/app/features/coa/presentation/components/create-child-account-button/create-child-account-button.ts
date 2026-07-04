91c879db47e6a81674596de55f4c6e26f1e7c2fc
José Comparotto <jose.comparotto@gmail.com>
2026-07-04 18:17:14 -0300

    feat: create child account

--- apps/web/src/app/features/coa/presentation/components/create-child-account-button/create-child-account-button.ts
+++ apps/web/src/app/features/coa/presentation/components/create-child-account-button/create-child-account-button.ts
@@ -1,9 +1,12 @@
-import { Component, input } from "@angular/core";
+import { Component, inject, input } from "@angular/core";
 import { NgIcon, provideIcons } from "@ng-icons/core";
 import { lucidePlus } from "@ng-icons/lucide";
 import { HlmButtonImports } from "@spartan-ng/helm/button";
 import { AccountEntity } from "@repo/coa-core";
 import { ExpandOnGroupHoverDirective } from "@libs/ui/directives/expand-on-group-hover.directive";
+import { AccontFormDialog, AccontFormDialogContext, AccontFormDialogResult } from "../account-form-dialog/account-form-dialog";
+import { HlmDialogService } from "@libs/ui/components/dialog/src";
+import { CoaFacade } from "../../facades/coa.facade";
 
 @Component({
     selector: 'app-create-child-account-button',
@@ -20,10 +23,30 @@ import { ExpandOnGroupHoverDirective } from "@libs/ui/directives/expand-on-group
     ],
 })
 export class CreateChildAccountButton {
+    private readonly hlmDialogService = inject(HlmDialogService);
+    private facade = inject(CoaFacade);
 
     account = input.required<Readonly<AccountEntity>>();
 
-    createChild() {
-        console.log('Create child account');
+    openDialog() {
+        this.hlmDialogService.open<AccontFormDialogContext, AccontFormDialogResult>(
+            AccontFormDialog, {
+            context: {
+                mode: 'create',
+                parent: this.account()
+            },
+        }).closed$.subscribe((result) => {
+            if (result) this.createChild(result);
+        })
+    }
+
+    private createChild(result: AccontFormDialogResult) {
+        this.facade.createChildAccount({
+            parentId: this.account().id,
+            name: result.name,
+            description: result.description,
+            isContra: result.isContra,
+            isSummary: result.isSummary,
+        })
     }
 }
